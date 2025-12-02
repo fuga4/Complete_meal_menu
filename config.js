8f6fdd7c1ba8d37fa8e9cce9123808e3f68a2e10
fuga4 <nkttnk2022@gmail.com>
2025-12-02 09:42:42 +0900

    config.js を作成

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDLcLq8M7UWBuDrRbq7OiFTmrBqy38hiKk",
+    authDomain: "complete-meal-menu.firebaseapp.com",
+    projectId: "complete-meal-menu",
+    storageBucket: "complete-meal-menu.firebasestorage.app",
+    messagingSenderId: "75250581224",
+    appId: "1:75250581224:web:cb8bca1f22dcd66b38d35d"
+  };
